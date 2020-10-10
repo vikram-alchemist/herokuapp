3d9e2d6ae4f0d3bbcef2216e9a0e554c4a337976
vikram-alchemist <hiwale.vikram07@gmail.com>
2020-10-10 22:49:30 +0530

    Testing url

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var http = require('http');
+const http = require('http');
+const url = require('url')
 
 http.createServer(function (req, res) {
     res.setHeader("Access-Control-Allow-Origin","*");
     res.writeHead(200, {'Content-Type': 'application/json'});
-    //res.write('Hello World!');
-    res.end(JSON.stringify({
-        name: "Viram"
-    }));
+    
+    const parsedUrl = url.parse(req.url)
+    
+    res.end(JSON.stringify(parsedUrl));
 }).listen(process.env.PORT || 8080 )
